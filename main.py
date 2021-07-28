d39269542176b286175bf5de5b6791d478a00241
ilkerozturk <ilkerozturketu@gmail.com>
2021-07-28 17:37:39 +0300

    Initial commit - this is a Python String exercise for me to remember Python

---
+++ main.py
@@ -0,0 +1,11 @@
+# This is an exercise for me to remember python string & concetenations
+
+adjective = input("Adjective is : ")
+verb1 = input("Verb : ")
+verb2 = input("Verb : ")
+famous_person = input("Famous person is:")
+
+madlib = f"Computer programming is so {adjective} !. It makes me so excited all the time because\
+  I love to {verb1}. Stay hydrated and {verb2} like you are a {famous_person}!"
+
+print(madlib)
